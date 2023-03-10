912ac29d5f70b2bc3c1b109e063972d7e5e9a5ab
Ethan7144 <ejtarlov@me.com>
2023-03-10 00:21:21 -0800

    Added Ping command file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Require the necessary discord.js classes
+const fs = require('node:fs');
+const path = require('node:path');
 require('dotenv').config();
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const { token } = process.env.DISCORD_TOKEN;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+client.commands = new Collection();
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
