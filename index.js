368753aaf57d339322a8578ce61ac016268ae515
Ethan7144 <ejtarlov@me.com>
2023-03-10 07:02:11 -0800

    moved log in confirmation for bot to events/ready.js from index.js

--- index.js
+++ index.js
@@ -46,11 +46,6 @@ client.on(Events.InteractionCreate, async interaction => {
 		}
 	}
 });
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
 
 // Log in to Discord with your client's token
 client.login(token);
