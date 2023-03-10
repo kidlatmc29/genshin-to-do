3a17d472cd6d8b7938f8b9a6f17b7ea59a0f221d
Ethan7144 <ejtarlov@me.com>
2023-03-10 07:04:50 -0800

    Moved the listener from index.js to events/interactionCreate.js

--- index.js
+++ index.js
@@ -25,27 +25,7 @@ for (const file of commandFiles) {
 	}
 }
 
-// creating a listener
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-	const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
 
 // Log in to Discord with your client's token
 client.login(token);
